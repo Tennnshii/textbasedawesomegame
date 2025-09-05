07424d53c20c3ccb67bb7301f9c6d427c98db469
Tennnshii <azali3290@gmail.com>
2025-09-04 23:19:04 -0500

    Update TextBasedGame.py

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -1,6 +1,3 @@
-print("Hello, World!")
-
-
 player_name = "Rudeus"
 player_hp = 100
 player_attack = 15
@@ -12,5 +9,13 @@ enemy_hp = 30
 enemy_attack = 5
 
 
+def player_attacks_enemy(playerAttack, enemyHP):
+    new_enemy_hp = enemyHP - playerAttack
+    if new_enemy_hp < 0: new_enemy_hp = 0
+    print(f"enemy took {player_attack} damage")
+    if new_enemy_hp == 0: print ("enemy is fucked")
+    return new_enemy_hp
 
 
+
+enemy_hp = player_attacks_enemy (player_attack, enemy_hp)
