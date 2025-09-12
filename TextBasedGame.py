ace932f3806e0b68ab518fa85a45c4c60a6e6090
Tennnshii <azali3290@gmail.com>
2025-09-11 23:08:46 -0500

    Update TextBasedGame.py

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -13,9 +13,25 @@ def player_attacks_enemy(playerAttack, enemyHP):
     new_enemy_hp = enemyHP - playerAttack
     if new_enemy_hp < 0: new_enemy_hp = 0
     print(f"enemy took {player_attack} damage")
-    if new_enemy_hp == 0: print ("enemy is fucked")
+    if new_enemy_hp == 0: print ("enemy is dead")
+    return new_enemy_hp
+
+def slash(playerAttack, player_mp, enemyHP):
+    new_enemy_hp = enemyHP - playerAttack * 1.25
+    player_mp = player_mp - 10
+    if new_enemy_hp < 0: new_enemy_hp = 0
+    print(f"You used Slash! Enemy took {player_attack * 1.25} damage")
+    if new_enemy_hp == 0: print ("enemy is dead")
     return new_enemy_hp
 
 
 
-enemy_hp = player_attacks_enemy (player_attack, enemy_hp)
+
+playerinput = int(input("Press 1 to attack,\n2 to slash. "))
+
+if playerinput == 1: 
+    enemy_hp = player_attacks_enemy (player_attack, enemy_hp)
+elif playerinput == 2:
+    enemy_hp = slash(player_attack, player_mp, enemy_hp)
+else: 
+    print("Invalid Entry")
